a8e3c1144c7e3011882bc5dfd010632effc9b823
SaNiA2901 <99397260+SaNiA2901@users.noreply.github.com>
2025-09-07 14:43:38 +0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,35 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { TradingStoreProvider } from "@/store/TradingStore";
+import AuthWrapper from "@/components/ui/AuthWrapper";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-const App = () => (
+const App = () => {
+  console.log('App component rendering...');
+  
+  return (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
+    <TradingStoreProvider>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <AuthWrapper>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </AuthWrapper>
+      </TooltipProvider>
+    </TradingStoreProvider>
   </QueryClientProvider>
-);
+  );
+};
 
 export default App;
